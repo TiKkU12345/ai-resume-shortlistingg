10a804a713dae4fb838508b34fde2a318338389e
Arnav Kumar <arunav.jsr.0604@gmail.com>
2025-11-12 23:22:24 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -19,6 +19,16 @@ from email_validator import validate_email
 # Run: python -m spacy download en_core_web_sm
 nlp = spacy.load("en_core_web_sm")
 
+import spacy
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
+
 class ResumeParser:
     """
     resume parser that extracts structured information
